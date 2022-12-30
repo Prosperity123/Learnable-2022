fa5a2c0bbed11439fe0bc2573c3ce580375c2c89
Prosperity123 <111201836+Prosperity123@users.noreply.github.com>
2022-12-30 23:48:39 +0100

    Add files via upload

---
+++ Teachable LMS.js
@@ -0,0 +1,62 @@
+const LearnableLMS = []
+
+class TeachableAdmin {
+  constructor(title) {
+    this.title = title;
+    this.Courses = []
+    this.rented = []
+  }
+
+  add(Courses = {}) {
+    this.Courses.push(Courses);
+  }
+
+ 
+  }
+
+  run () {
+
+  }
+
+const newBook = new Book('TFA')
+
+
+
+library.add(newBook)
+library.add(new Book('Mastery'))
+
+console.log(library, '\n\n',libraryee, '\n\n', libraryew)
+
+// console.log(thingsFallApart)
+
+
+const Teachable = class Learnable{
+        constructor(title){
+                    this.title = title;
+                    this.name = "Learnable";
+                    this.label = "Learnable";
+                    this.description = "Learnable";
+                    this.i = 0;
+
+                    this.title.teachables.push(this); 
+                    this.app.teachables.sort((a, b) => {
+                        if(a.name < b.name){
+                            return -1; 
+
+                    
+                })
+            }
+        
+}   const Genies extends Teachable{
+                    constructor(app){
+                        super(app);
+                        this.name = "Genies";
+                        this.label = "Genies";
+                        this.description = "Genies";
+                        this.i = 0;
+                        this.app.teachables.push(this);
+                        this.app.teachables.sort((a, b) => {
+                            if(a.name < b.name){
+                                return -1;
+
+}
